bb7a2099ea5145d8440e692974076b7fb2042a02
bodwood <bodiewood@hotmail.com>
2022-12-13 13:19:13 -0800

    Add clear list methods
    Co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- DealershipApp/Controllers/HomeController.cs
+++ DealershipApp/Controllers/HomeController.cs
@@ -1,29 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using DealershipApp.Models;
 using System.Collections.Generic;
- 
+
 namespace DealershipApp.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index() {
-      List<Cars> allCars = Cars.GetAll();
-      return View(allCars);
-    }
-
-    [HttpGet("/car/new")]
-    public ActionResult CreateForm()
+    public class HomeController : Controller
     {
-      return View();
-    }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            List<Cars> allCars = Cars.GetAll(); //static allows this to occur. No new instance is needed for car
+            return View(allCars);
+        }
+        [HttpGet("/clearform")]
+        public ActionResult ClearForm()
+        {
+            List<Cars> allCars = Cars.GetAll(); //static allows this to occur. No new instance is needed for car
+            return View(allCars);
+        }
 
-    [HttpPost("/car")]
-    public ActionResult Create(string name, string make, string model)
-    {
-      string description = String.Format("Name: {0}, Make: {1}, Model: {2}", name, make, model);
-      Cars myCars = new Cars(description);
-      return RedirectToAction("Index");
+        [HttpGet("/car/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/car/add")]
+        public ActionResult Create(string name, string make, string model)
+        {
+            string description = String.Format("Name: {0}, Make: {1}, Model: {2}", name, make, model);
+            Cars myCars = new Cars(description);
+            return RedirectToAction("Index");
+        }
+        [HttpPost("/car/delete")]
+        public ActionResult Clear()
+        {
+            Cars.ClearAll();
+            return RedirectToAction("ClearForm");
+        }
     }
-  }
 }
